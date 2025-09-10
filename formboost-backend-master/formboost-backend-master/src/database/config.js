56002250d0becd1ebff37f741b42cf7d9c75c708
Ridham-Savaliya <savaliyaridham46@.email.com>
2025-09-10 21:47:25 +0530

    .

--- formboost-backend-master/formboost-backend-master/src/database/config.js
+++ formboost-backend-master/formboost-backend-master/src/database/config.js
@@ -1,10 +1,13 @@
 import { Sequelize } from 'sequelize';
+import mysql2 from 'mysql2';
 import config from '#config/index.js';
 
 const sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {
   host: config.db.host,
   port: Number(config.db.port),
   dialect: config.db.dialect,
+  // This is the crucial line to fix the bundling issue with esbuild
+  dialectModule: mysql2,
   pool: {
     max: config.db.pool.max,
     min: config.db.pool.min,
